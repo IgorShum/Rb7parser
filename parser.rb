070821e8177492d589fa2df5b41266a0e5eb1e9f
IgorShum <Shumeikoigor@yandex.ru>
2021-03-05 16:31:38 +0300

    Parser RB7

---
+++ parser.rb
@@ -0,0 +1,43 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Rb7parser
+
+	attr_reader :top_movies
+
+	URL = 'https://rb7.ru/afisha/movies'.freeze
+
+
+	def initialize
+		@movies = []
+		top if load_html
+	end
+
+	def load_html
+		html = URI.open(URL)
+		puts html.inspect
+		doc = Nokogiri::HTML(html)
+		parse(doc)
+	end
+
+	def parse(html)
+		html.css('.movie').map do |m|
+			seanses = 0
+			title = m.at_css('h2 a').inner_html.strip
+			table = m.css('.afisha-schedule')
+			kinoteaters = table.search('tr').each do |tr|
+				seanses += tr.css("td").css('.when').css('.show').count
+			end
+			@movies << { title: title, seanses: seanses }
+		end
+	end
+
+	def top
+		@top_movies = @movies.sort_by{|key, _val| key[:seanses]}.last(3).reverse!
+	end
+end
+
+p = Rb7parser.new
+p.top_movies.each do |t|
+	puts "#{t[:title]} : #{t[:seanses]}"
+end
